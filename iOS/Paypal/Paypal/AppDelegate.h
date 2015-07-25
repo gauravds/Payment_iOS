7231e11b2c5e877a891b99f14d8f124d559d0fd7
GauravDS <gaurav.sharma@punchh.com>
2015-07-26 02:04:21 +0530

    Paypal payment integrating with card IO

---
+++ iOS/Paypal/Paypal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Paypal
+//
+//  Created by gauravds on 7/24/15.
+//  Copyright (c) 2015 GDS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS/Paypal/Paypal/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Paypal
+//
+//  Created by gauravds on 7/24/15.
+//  Copyright (c) 2015 GDS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
